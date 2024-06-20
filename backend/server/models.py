e87dcdc10093bcf132432742b7094348c69bae70
JeyasuryaUR <jeyasurya0206@gmail.com>
2024-06-20 17:37:22 +0530

    Include image url

--- backend/server/models.py
+++ backend/server/models.py
@@ -4,4 +4,5 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField()
     instructions = models.TextField()
-    category = models.CharField(max_length=200)  
+    category = models.CharField(max_length=200)
+    imageUrl = models.URLField(max_length=500, default='') 
